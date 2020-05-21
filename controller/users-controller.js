93e7472d1db11e087484a8093cf1c2ad6bab1f2d
Saneesh Kumar <saneeshnair@gmail.com>
2020-05-21 19:10:43 +0900

    Added Put method to edit the user

--- controller/users-controller.js
+++ controller/users-controller.js
@@ -90,6 +90,58 @@ class UsersController {
             user,
         });
     }
+
+    updateUser(req, res) {
+        const id = parseInt(req.params.id, 10);
+        let userFound;
+        let itemIndex;
+        users.map((user, index) => {
+          if (user.id === id) {
+            userFound = user;
+            itemIndex = index;
+          }
+        });
+
+        if (!userFound) {
+          return res.status(404).send({
+            success: 'false',
+            message: 'user not found',
+          });
+        }
+
+        if (!req.body.firstName) {
+            return res.status(400).send({
+                success: 'false',
+                message: 'firstName is required',
+            });
+        } else if (!req.body.secondName) {
+            return res.status(400).send({
+                success: 'false',
+                message: 'secondName is required',
+            });
+        }
+        else if (!req.body.age) {
+            return res.status(400).send({
+                success: 'false',
+                message: 'age is required',
+            });
+        }
+
+        const newuser = {
+          id: userFound.id,
+          firstName: req.body.firstName || userFound.firstName,
+          secondName: req.body.secondName || userFound.secondName,
+          age: req.body.age || userFound.age
+        };
+
+        users.splice(itemIndex, 1, newuser);
+
+        return res.status(201).send({
+          success: 'true',
+          message: 'user added successfully',
+          newuser,
+        });
+      }
 }
 
 module.exports = { UsersController };
--- routes/users-route.js
+++ routes/users-route.js
@@ -8,4 +8,5 @@ module.exports = app
 app.get("/api/v1/users", UsersController.prototype.getAllUsers);
 app.get("/api/v1/users/:id", UsersController.prototype.getUserById);
 app.post("/api/v1/users", UsersController.prototype.createUser);
+app.put("/api/v1/users/:id", UsersController.prototype.updateUser);
 app.delete("/api/v1/users/:id", UsersController.prototype.deleteUserById);
